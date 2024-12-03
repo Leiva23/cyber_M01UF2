04da0594e5bc230942deed4a4bcf7854cf86b571
Alex Leiva <alex.leiva@enti.cat>
2024-12-03 14:25:47 +0100

    Proj

--- client.sh
+++ client.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
+echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
+
+echo "1. SEND HEADER"
+
+echo "LSTP_1" | nc localhost 7777
+
+echo "2.LISTEN OK_HEADER"
+
+DATA=`nc -l 7777`
+
+echo "6.CHECK OK_HEADER"
+if [ "$DATA" != "OK_HEADER" ]
+then
+	echo "ERROR 1: Header enviado correctamente"
+
+	exit 1
+fi
+
+
+
+#cat client/lechuga.lechu | text2wave -o client/lechuga1.wav
+
+#yes | ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
+
+echo "FILE_NAME client/lechuga1.wav" | nc localhost 7777
+
+
+
+
+
 
--- server.sh
+++ server.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+echo "LSTP Server (Lechuga Speaker Transfer Protocol)"
+
+echo "0. LISTEN"
+
+DATA=`nc -l 7777`
+
+echo "3.CHECK"
+
+if [ "$DATA" != "LSTP_1" ]
+then
+	echo "ERROR 1: Header mal formado $DATA"
+
+	echo "KO_HEADER" | nc localhost 7777
+
+	exit 1
+fi
+
+echo "4.SEND OK_HEADER" 
+
+echo "OK_HEADER" | nc localhost 7777
+
+echo "5.LISTEN FILE_NAME"
+DATA=`nc -l 7777`
+
+
+
+
+
+
